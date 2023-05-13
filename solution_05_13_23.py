86d2b48260737ed56d45ec4973565a9834d5d58c
kjmyers <kevin.j.myers@gmail.com>
2023-05-13 09:27:23 -0700

    Create solution_05_13_23.py

---
+++ solution_05_13_23.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        mod = 10 ** 9 + 7
+        dp = [1] + [0] * (high)
+
+        for e in range(1, high+1):
+            if e >= zero:
+                dp[e] += dp[e - zero]
+            if e >= one:
+                dp[e] += dp[e - one]
+            dp[e] %= mod
+        
+        return sum(dp[low : high+1]) % mod
