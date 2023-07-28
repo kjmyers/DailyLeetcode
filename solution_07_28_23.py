3f66bc0f520fd94eb65a0948976fca8895d0dea0
kjmyers <kevin.j.myers@gmail.com>
2023-07-28 08:16:49 -0400

    Create solution_07_28_23.py

---
+++ solution_07_28_23.py
@@ -0,0 +1,19 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+
+        memo = collections.defaultdict(int)
+        
+        def maxDiff(left, right):
+            if (left, right) in memo:
+                return memo[(left, right)]
+            if left == right:
+                return nums[left]
+            
+            score_by_left = nums[left] - maxDiff(left + 1, right)
+            score_by_right = nums[right] - maxDiff(left, right - 1)
+            
+            memo[(left, right)] = max(score_by_left, score_by_right)
+            
+            return memo[(left, right)]
+        
+        return maxDiff(0, len(nums) - 1) >= 0
