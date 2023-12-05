4c3058fb7d1be89cded3c1e45271c814c3c903be
kjmyers <kevin.j.myers@gmail.com>
2023-12-05 09:51:25 -0500

    Create solution_12_05_23.py

---
+++ solution_12_05_23.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        
+        # ret = 0
+        # while n > 1:
+        #     cur, extra = divmod(n,2)
+        #     ret += cur
+        #     n = cur + extra
+        
+        # return ret
+        return n-1
