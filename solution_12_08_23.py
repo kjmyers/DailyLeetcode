a2b45b76b01074d27d1889dd24a0112c4a735bf0
kjmyers <kevin.j.myers@gmail.com>
2023-12-08 09:24:29 -0500

    Create solution_12_08_23.py

---
+++ solution_12_08_23.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        ret = str(root.val)
+        
+        if(root.left):
+            ret += "(" + self.tree2str(root.left) + ")"
+        
+        if(root.right):
+            if(not root.left):
+                ret += "()"
+            ret += "(" + self.tree2str(root.right) + ")"
+        
+        return ret
