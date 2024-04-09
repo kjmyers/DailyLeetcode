757edbe82a0c51aad40468b99d9067b3fef8154e
kjmyers <kevin.j.myers@gmail.com>
2024-04-09 09:13:14 -0400

    Create solution_04_09_24.py

---
+++ solution_04_09_24.py
@@ -0,0 +1,12 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        n = len(tickets)
+
+        time = 0
+        while tickets:
+            for i in range(n):
+                if tickets[i] > 0:
+                    tickets[i] -= 1
+                    time += 1
+                    if i == k and tickets[i] == 0:
+                        return time
