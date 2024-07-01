1406375857150fb1ce9958f66d8b129558fc56fb
kjmyers <kevin.j.myers@gmail.com>
2024-07-01 13:59:52 -0400

    Create solution_07_01_24.py

---
+++ solution_07_01_24.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odds = 0
+        for n in arr:
+            if n % 2 == 1:
+                odds += 1
+                if odds == 3:
+                    return True
+            else:
+                odds = 0
+        
+        return False
