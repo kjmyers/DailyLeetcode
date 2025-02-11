775851e0c32ef96e357c23380bca970b98b97888
kjmyers <kevin.j.myers@gmail.com>
2025-02-11 09:08:24 -0500

    Create solution_02_11_25.py

---
+++ solution_02_11_25.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        n = len(part)
+
+        for c in s:
+            stack.append(c)
+            if len(stack) >= n and "".join(stack[-n:]) == part:
+                for _ in range(n):
+                    stack.pop()
+        
+        return "".join(stack)
