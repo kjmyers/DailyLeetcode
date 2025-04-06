12f5f510c9932e5a920aa99e093ce8d70535588d
kjmyers <kevin.j.myers@gmail.com>
2025-04-06 10:20:37 +0900

    Update solution_04_05_25.py

--- solution_04_05_25.py
+++ solution_04_05_25.py
@@ -1,31 +1,9 @@
 class Solution:
-    def subsetXORSum(self, nums: List[int]) -> int:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         
-        def generate_subsets(nums, index, subset, subsets):
-            # Base case: index reached end of nums
-            # Add the current subset to subsets
-            if index == len(nums):
-                subsets.append(subset[:])
-                return
-
-            # Generate subsets with nums[i]
-            subset.append(nums[index])
-            generate_subsets(nums, index + 1, subset, subsets)
-            subset.pop()
-
-            # Generate subsets without nums[i]
-            generate_subsets(nums, index + 1, subset, subsets)
-
-        # Generate all of the subsets
-        subsets = []
-        generate_subsets(nums, 0, [], subsets)
-
-        # Compute the XOR total for each subset and add to the result
-        result = 0
-        for subset in subsets:
-            subset_XOR_total = 0
-            for num in subset:
-                subset_XOR_total ^= num
-            result += subset_XOR_total
-
-        return result
+        subsets = {-1: set()}
+        
+        for num in sorted(nums):
+            subsets[num] = max([subsets[k] for k in subsets if num % k == 0], key=len) | {num}
+        
+        return list(max(subsets.values(), key=len))
