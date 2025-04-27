6dc33cd8334c71ba1adf7b91d72f41715ac2a661
kjmyers <kevin.j.myers@gmail.com>
2025-04-27 00:04:49 -0400

    Create solution_04_27_25.py

---
+++ solution_04_27_25.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        ret = 0
+        for i in range(len(nums) - 2):
+            if nums[i] + nums[i+2] == nums[i+1] / 2:
+                ret += 1
+        
+        return ret
