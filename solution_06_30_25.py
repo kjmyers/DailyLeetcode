d0cb4b88a2997b87ecb6a2d335cb3e6a94f510b9
kjmyers <kevin.j.myers@gmail.com>
2025-06-30 09:27:15 -0400

    Create solution_06_30_25.py

---
+++ solution_06_30_25.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        nums.sort()
+        ret = 0
+
+        l = 0
+        for r in range(len(nums)):
+            while nums[r] - nums[l] > 1:
+                l += 1
+            if nums[r] - nums[l] == 1:
+                ret = max(ret, r - l + 1)
+
+        return ret
