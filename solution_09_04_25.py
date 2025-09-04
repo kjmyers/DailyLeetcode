3a97597086835660f323d9cb4450975c9ffd670c
kjmyers <kevin.j.myers@gmail.com>
2025-09-04 00:17:52 -0400

    Create solution_09_04_25.py

---
+++ solution_09_04_25.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(y-z) > abs(x-z):
+            return 1
+        elif abs(y-z) < abs(x-z):
+            return 2
+        else:
+            return 0
