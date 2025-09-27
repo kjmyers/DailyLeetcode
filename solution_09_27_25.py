30e2a01adfe05197afb29ce303ac8e98160bb8b1
kjmyers <kevin.j.myers@gmail.com>
2025-09-27 00:17:54 -0400

    Create solution_09_27_25.py

---
+++ solution_09_27_25.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        n = len(points)
+        maxArea = 0
+        for i in range(n-2):
+            x1,y1 = points[i]
+            for j in range(i+1,n-1):
+                x2,y2 = points[j]
+                for k in range(j+1,n):
+                    x3,y3 = points[k]
+                    maxArea = max(maxArea, 0.5 * abs( x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) )  )
+        return maxArea
