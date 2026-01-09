850a2959932cfeb4e7d9f050790664d4034ab2e8
kjmyers <kevin.j.myers@gmail.com>
2026-01-09 12:04:51 -0500

    Create solution_01_09_26.py

---
+++ solution_01_09_26.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        depth = {None: -1}
+        # Find maximum depth via DFS
+        def dfs(node, parent = None):
+            if node:
+                depth[node] = depth[parent] + 1
+                dfs(node.left, node)
+                dfs(node.right, node)
+        dfs(root)
+        max_depth = max(depth.values())
+
+        def answer(node):
+            # Return the answer for the subtree at node.
+            if not node or depth.get(node, None) == max_depth:
+                return node
+            L, R = answer(node.left), answer(node.right)
+            return node if L and R else L or R
+
+        return answer(root)
