0eaf6f5afc790ca53fab0824f83375406f5e4e07
kjmyers <kevin.j.myers@gmail.com>
2026-09-24 08:53:38 -0400

    Implement smallestIndex method in Solution class

---
+++ solution_09_24_26.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i, num in enumerate(nums):
+            total = 0
+            while num > 0:
+                total += num % 10
+                num //= 10
+            if total == i:
+                return i
+        
+        return -1
